2d34e5c34d637b92baad66dc57a5dcc851edaca9
jos8304 <jos8304@hotmail.com>
2024-04-04 21:23:48 -0400

    create post models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    #title, content, create_date,updated_date
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    dt_created = models.DateField(verbose_name="Date Created", auto_now_add=True)
+    dt_modified = models.DateField(verbose_name="Date Modified", auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
