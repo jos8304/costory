cf31c9052718943faa1699a971e7ea3c71f15882
jos8304 <jos8304@hotmail.com>
2024-04-22 22:14:51 -0400

    add built-in validator and custom validator

--- posts/models.py
+++ posts/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from .validators import validate_symbols
 
 # Create your models here.
 class Post(models.Model):
     #title, content, create_date,updated_date
-    title = models.CharField(max_length=50)
-    content = models.TextField()
+    title = models.CharField(max_length=50, unique=True, error_messages={'unique': 'This title already exists.'})
+    content = models.TextField(validators=[MinLengthValidator(10, 'Please write at least 10 characters.'), validate_symbols])
     dt_created = models.DateField(verbose_name="Date Created", auto_now_add=True)
     dt_modified = models.DateField(verbose_name="Date Modified", auto_now=True)
 
