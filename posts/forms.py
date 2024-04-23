1eb7369c227849e195939f4959004ebd89f4936c
jos8304 <jos8304@hotmail.com>
2024-04-22 21:48:34 -0400

    create model form

--- posts/forms.py
+++ posts/forms.py
@@ -1,5 +1,9 @@
 from django import forms
+from .models import Post
 
-class PostForm(forms.Form):
-    title = forms.CharField(max_length=50, label="Title")
-    content = forms.CharField(label="content", widget=forms.Textarea)
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+
+        fields = ['title', 'content']
+    
