1ed02162d3cf495b735359285ad40aef0f1449e5
Jason <jyou8304@gmail.com>
2024-04-24 16:27:34 -0400

    add validation in form.py

--- posts/forms.py
+++ posts/forms.py
@@ -1,12 +1,20 @@
 from django import forms
 from .models import Post
 from .validators import validate_symbols
+from django.core.exceptions import ValidationError
+
 
 
 class PostForm(forms.ModelForm):
-    memo = forms.CharField(max_length=80, validators=[validate_symbols])
     class Meta:
         model = Post
 
         fields = ['title', 'content']
     
+    def clen_title(self):
+        title = self.cleaned_data['title']
+        if '*' in title:
+            raise ValidationError('* cannot be included.')
+        
+        return title
+
