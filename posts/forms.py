bb046dd5ed545bfd7668a87dbadb4b192ebbd72f
Jason <jyou8304@gmail.com>
2024-04-24 16:22:49 -0400

    add validation in form.py

--- posts/forms.py
+++ posts/forms.py
@@ -1,7 +1,10 @@
 from django import forms
 from .models import Post
+from .validators import validate_symbols
+
 
 class PostForm(forms.ModelForm):
+    memo = forms.CharField(max_length=80, validators=[validate_symbols])
     class Meta:
         model = Post
 
